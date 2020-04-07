7a5a03fff86d817d4a032a532a49939345e28ea7
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-04-07 20:27:22 +0200

    Zwischenstand

--- app/src/main/java/com/example/erratedenvip/MainActivity.java
+++ app/src/main/java/com/example/erratedenvip/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.erratedenvip;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 
@@ -11,11 +14,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button0, button1, button2, button3;
     ImageView imageView;
+    ArrayList<String> celebURLs = new ArrayList<String>();
+    ArrayList<String> celebNames = new ArrayList<String>();
+    int chosenCeleb = 0;
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
 
@@ -46,6 +57,39 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public class ImageDownloader extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmap;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+
+    public void newQuestion() {
+        try {
+            Random rand = new Random();
+
+            chosenCeleb = rand.nextInt(celebURLs.size());
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,5 +103,32 @@ public class MainActivity extends AppCompatActivity {
 
         DownloadTask task = new DownloadTask();
         String result = null;
+
+        try {
+            result = task.execute("http://www.posh24.se/kandisar").get();
+
+            String[] splitResult = result.split("<div class=\"listedArticles\">");
+
+            Pattern p = Pattern.compile("<img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+                celebURLs.add(m.group(1));
+                Log.i("StarbuildURL ", m.group(1));
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()) {
+                celebNames.add(m.group(1));
+                Log.i("StarbuildName ", m.group(1));
+            }
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
