eeeccdfbd8f130652d8dd9acb1d01f6841678abb
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-04-07 15:15:23 +0200

    Layout fertig Downloadklasse fertig

--- app/src/main/java/com/example/erratedenvip/MainActivity.java
+++ app/src/main/java/com/example/erratedenvip/MainActivity.java
@@ -1,14 +1,63 @@
 package com.example.erratedenvip;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button button0, button1, button2, button3;
+    ImageView imageView;
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(inputStream);
+                int data = reader.read();
+
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        imageView = findViewById(R.id.imageView);
+
+        DownloadTask task = new DownloadTask();
+        String result = null;
     }
 }
